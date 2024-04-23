cc6014f01c3cefa968b1eba163be178922c03b8b
Nitesh Mali <71972708+imnitesh18@users.noreply.github.com>
2024-04-24 02:17:46 +0530

    Update urls.py

--- FoodProject/urls.py
+++ FoodProject/urls.py
@@ -46,5 +46,5 @@ urlpatterns = [
 ]
 
 # Static and media files serving during development
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
