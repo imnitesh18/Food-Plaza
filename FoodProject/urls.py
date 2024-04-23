1f39fff71c7ca0555d29b854097833e1d10ce977
Nitesh Mali <niteshmali@nitesh-mali.local>
2024-04-24 02:03:18 +0530

    update urls.py

--- FoodProject/urls.py
+++ FoodProject/urls.py
@@ -1,64 +1,50 @@
-"""FoodProject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from foodapp import views
-from django.conf import settings
-from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-        # ---- HOME ---- 
-        path('',views.foodapp), 
-        path('home',views.foodapp), 
+    # Home
+    path('', views.foodapp, name='home'), 
+    path('home', views.foodapp, name='home'), 
             
-        # ---- FOOD ---- 
-        path('addfoods',views.addfood),
-        path('deletefood/<int:FoodId>',views.deletefood),
-        path('getfood/<int:FoodId>',views.getfood),
-        path('editfood/<int:FoodId>',views.updatefood),
-        path('allfood',views.showfood),
+    # Food
+    path('addfoods', views.addfood, name='addfoods'),
+    path('deletefood/<int:FoodId>', views.deletefood, name='deletefood'),
+    path('getfood/<int:FoodId>', views.getfood, name='getfood'),
+    path('editfood/<int:FoodId>', views.updatefood, name='editfood'),
+    path('allfood', views.showfood, name='allfood'),
             
-        # ---- CUSTOMER ---- 
-        path('addcustomer',views.addcust),
-        path('deletecustomer/<int:CustId>',views.deletecust),
-        path('getcustomer',views.getcust),
-        path('editcustomer/<int:CustId>',views.updatecust),
-        path('allcustomer',views.showcust),
+    # Customer
+    path('addcustomer', views.addcust, name='addcustomer'),
+    path('deletecustomer/<int:CustId>', views.deletecust, name='deletecustomer'),
+    path('getcustomer', views.getcust, name='getcustomer'),
+    path('editcustomer/<int:CustId>', views.updatecust, name='editcustomer'),
+    path('allcustomer', views.showcust, name='allcustomer'),
     
-        # ---- CUSTOMER ---- 
-        path('updatepasswd',views.updatepasswd),
-        path('updatepassword',views.changepass),
+    # Update Password
+    path('updatepasswd', views.updatepasswd, name='updatepasswd'),
+    path('updatepassword', views.changepass, name='updatepassword'),
     
-        # ---- LOGIN ---- 
-        path('login',views.login),
-	path('dologin',views.doLogin),
-	path('logout',views.doLogout),
+    # Login
+    path('login', views.login, name='login'),
+    path('dologin', views.doLogin, name='dologin'),
+    path('logout', views.doLogout, name='logout'),
 	
-	# ---- CART ---- 
-	path('addtocart/<int:FoodId>',views.addcart),
-	path('allcart',views.showcart),
-	path('deletecart/<int:CartId>',views.delcart),
-	path('updateqnty/<str:s>',views.updateQNT),
+    # Cart
+    path('addtocart/<int:FoodId>', views.addcart, name='addtocart'),
+    path('allcart', views.showcart, name='allcart'),
+    path('deletecart/<int:CartId>', views.delcart, name='deletecart'),
+    path('updateqnty/<str:s>', views.updateQNT, name='updateqnty'),
 	
-	# ---- ORDER ---- 
-	path('placeorder',views.placeorder),
-	path('orders',views.getorder),
+    # Order
+    path('placeorder', views.placeorder, name='placeorder'),
+    path('orders', views.getorder, name='orders'),
 ]
+
+# Static and media files serving during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
