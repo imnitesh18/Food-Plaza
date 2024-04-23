1ecfd277358c3bc5dadc7121c28a964ceca1bb62
Nitesh Mali <niteshmali@nitesh-mali.local>
2024-04-24 00:54:24 +0530

    settings.y

--- FoodProject/settings.py
+++ FoodProject/settings.py
@@ -129,7 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'foodapp', 'static','css')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'foodapp' 'static')
 
 
